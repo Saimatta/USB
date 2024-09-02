6f1b738a5d27c4d8e68a60e00389d5fe4b503c4b
Saimatta <99985390+Saimatta@users.noreply.github.com>
2024-09-02 12:04:30 +0000

    Update Ternary.java

--- Ternary.java
+++ Ternary.java
@@ -2,7 +2,7 @@ public class Ternary{
   public static void main(String[] args){
     int age=20;
     boolean b=age>18?true:false;
-    System.out.println("The entered age is : sai "+age);
+    System.out.println("The entered age is : "+age);
     System.out.println(b);
   }
 }
