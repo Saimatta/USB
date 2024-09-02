1bb39bfd816553e4c33d884ffaabb638d935f813
Saimatta <99985390+Saimatta@users.noreply.github.com>
2024-09-02 11:26:01 +0000

    Update Display.java

--- Display.java
+++ Display.java
@@ -1,7 +1,7 @@
 public class Display{
   public static void main(String[] args){
+    System.out.println("The multiplication table for 6 is : ");
     for(int i=1;i<=10;i++){
-      System.out.println("The multiplication table for 6 is : ");
       System.out.println("6 * "+i+" is : "+(6*i));
     }
   }
